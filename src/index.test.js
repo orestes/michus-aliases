75c55ebfad3d5806702f0855df136abb55e37d13
Orestes Carracedo <orestes.ca@gmail.com>
2016-01-07 13:12:50 +0100

    Setup testing

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var aliases = require('./index');
+
+describe('michus-aliases', function () {
+    it('should be testable', function () {
+        expect(true).to.be.equal(true);
+    });
+    it('should be testable', function () {
+        expect(true).to.not.be.equal(false);
+    })
+});
